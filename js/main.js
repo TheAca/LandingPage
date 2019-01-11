ee8bdb375357270dd6044bfeca30a46e550dc567
DESKTOP-D387SFC\bogoj <aleksandarbogojevic97@gmail.com>
2019-01-11 19:10:11 +0100

    Landing Page 1
    This site was made with me and one of my friends.

---
+++ js/main.js
@@ -0,0 +1,56 @@
+window.sr = ScrollReveal();
+
+sr.reveal ('.banner-box', {
+    duration: 2000,
+    origin: 'top',
+    distance: '300px'
+});
+sr.reveal ('.showcase-right', {
+    duration: 2800,
+    origin: 'right',
+    distance: '500px',
+    viewFactor: 0.3
+});sr.reveal ('.showcase-right01', {
+    duration: 2800,
+    delay:  800,
+    origin: 'right',
+    distance: '500px',
+    viewFactor: 0.3
+});
+sr.reveal ('.showcase-right02', {
+    duration: 2800,
+    delay:  1400,
+    origin: 'right',
+    distance: '500px',
+    viewFactor: 0.3
+});
+sr.reveal ('.showcase-right2', {
+    duration: 1500,
+    origin: 'right',
+    distance: '500px',
+    viewFactor: 0.3
+});
+sr.reveal ('.showcase-left', {
+    duration: 2800,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.3
+});
+sr.reveal ('.showcase-left2', {
+    duration: 1500,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.3
+});
+sr.reveal ('.bottom', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '500px',
+    viewFactor: 0.3
+});
+sr.reveal ('.top', {
+    duration: 2000,
+    origin: 'top',
+    distance: '500px',
+    viewFactor: 0.3
+});
